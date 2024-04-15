d087b906b714e679b61bf98ffbb795d923722257
Young <rhsiddlsidhd@nate.com>
2024-04-15 20:53:36 +0900

    merge test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/">
+          {" "}
+          {/* Header */}
+          <Route index /> {/* MainPage*/}
+          <Route path="search_cafes">
+            {" "}
+            {/* SearchPage */} <Route index />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

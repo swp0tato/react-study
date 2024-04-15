47386fbceb3da6e409831574caa4c289c19b1003
Young <rhsiddlsidhd@nate.com>
2024-04-15 21:23:42 +0900

    router setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./layout/Header";
+import MainPage from "./pages/main/MainPage";
+import SearchPage from "./pages/search/SearchPage";
+import Detail from "./pages/detail/Detail";
+import Board from "./pages/board/Board";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/">
-          {" "}
-          {/* Header */}
-          <Route index /> {/* MainPage*/}
-          <Route path="search_cafes">
-            {" "}
-            {/* SearchPage */} <Route index />
+        <Route path="/" element={<Header />}>
+          <Route element={<MainPage />}>
+            <Route path="search">
+              <Route index element={<SearchPage />} />
+              <Route path=":id" element={<Detail />} />
+            </Route>
+            <Route path="board" element={<Board />} />
           </Route>
         </Route>
       </Routes>

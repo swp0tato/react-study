e2b1c431f1c0219603eeb1397621754219b07133
hayoung <ha02e@naver.com>
2024-04-15 21:46:20 +0900

    WeatherDessertSlide 레이아웃 짜는 중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./layout/AppLayout/AppLayout";
+import Hompage from "./pages/Homepage/Hompage";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Hompage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Homepage/Hompage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CloseDessertSlide from "./components/CloseDessertSlide/CloseDessertSlide";
+import WeatherDessertSlide from "./components/WeatherDessertSlide/WeatherDessertSlide";
+
+const Hompage = () => {
+  return (
+    <div>
+      Hompage
+      <CloseDessertSlide />
+      <WeatherDessertSlide />
+    </div>
+  );
+};
+
+export default Hompage;

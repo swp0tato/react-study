853199363229e4d22b63358f4e66047e26c57ba6
minsol <rlawlgus3954@naver.com>
2024-04-15 22:11:45 +0900

    커밋테스트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Header from "./layout/Header";
-import MainPage from "./pages/main/MainPage";
-import SearchPage from "./pages/search/SearchPage";
-import Detail from "./pages/detail/Detail";
-import Board from "./pages/board/Board";
+import { Outlet, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './layout/Header';
+import MainPage from './pages/main/MainPage';
+import SearchPage from './pages/search/SearchPage';
+import Detail from './pages/detail/Detail';
+import Board from './pages/board/Board';
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
             <Route path="board" element={<Board />} />
           </Route>
         </Route>
+        <Outlet />
       </Routes>
     </div>
   );
--- src/pages/board/Board.jsx
+++ src/pages/board/Board.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import './Board.style.css';
 
 const Board = () => {
-  return <div>Board</div>;
+  console.log('board page!');
+  return (
+    <div>
+      Board
+      <h2>왜 안보이지?</h2>
+    </div>
+  );
 };
 
 export default Board;

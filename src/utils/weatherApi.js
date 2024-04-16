f7bcc5d73c0078a7e6751e9aab9f71929aeaef4f
hayoung <ha02e@naver.com>
2024-04-16 14:57:28 +0900

    weatherApi.js 추가

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,1 +1,32 @@
 import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+console.log("weather api 키??", API_KEY);
+
+const weatherApi = axios.create({
+  baseURL: "https://api.openweathermap.org/data/2.5/weather",
+  headers: {
+    Accept: "application/json",
+    Authorization: API_KEY,
+  },
+});
+
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default weatherApi;

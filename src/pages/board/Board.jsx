4425ec56a33b5d6f583f5672c69ecf60cfbedb56
minsol <rlawlgus3954@naver.com>
2024-04-16 16:20:24 +0900

    카드이미지수정

--- src/pages/board/Board.jsx
+++ src/pages/board/Board.jsx
@@ -12,7 +12,7 @@ const Board = () => {
   console.log('board page!');
 
   return (
-    <div>
+    <div className="board-wrap">
       <h3 className="board-title">디저트 추천 🧀</h3>
       <div className="add-btn-wrap">
         <button onClick={addBoard} className="add-btn">

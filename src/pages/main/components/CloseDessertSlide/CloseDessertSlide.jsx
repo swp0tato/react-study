07da9e988d982de3162539e4243bedfcb17ec10a
hayoung <ha02e@naver.com>
2024-04-16 12:09:57 +0900

    가까운 디저트 맛집 Card 컴포넌트 가져와 스타일 변경

--- src/pages/main/components/CloseDessertSlide/CloseDessertSlide.jsx
+++ src/pages/main/components/CloseDessertSlide/CloseDessertSlide.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "./../../../../common/Card/Card";
 import "./CloseDessertSlide.style.css";
 
 const CloseDessertSlide = () => {
@@ -6,6 +7,16 @@ const CloseDessertSlide = () => {
     <div className="close_dessert_section">
       <div className="close_dessert_wrapper">
         <h2>가까운 디저트 맛집 Top5</h2>
+        <div className="slider">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+      {/* <div className="close_dessert_wrapper">
         <div className="slide">
           <div className="close_img"></div>
           <div className="close_name">스타벅스 남이섬점</div>
@@ -13,7 +24,7 @@ const CloseDessertSlide = () => {
             경기 가평군 가평읍 북한강변로 1054
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };

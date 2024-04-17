e4dabfc40908a9711b03045a478ad125ef0b256a
hayoung <ha02e@naver.com>
2024-04-17 15:28:40 +0900

    날씨에 따라 디저트 키워드 작성

--- src/pages/main/components/WeatherDessertSlide/WeatherDessertSlide.jsx
+++ src/pages/main/components/WeatherDessertSlide/WeatherDessertSlide.jsx
@@ -16,49 +16,68 @@ const WeatherDessertSlide = ({ lat, lon }) => {
     return <div>Loading...</div>;
   }
   if (isError) {
-    <div>Loading...</div>;
+    return <div>{error.message}</div>;
   }
 
-  if (data) {
-    //날씨에 따라 백그라운드 이미지 변경
-    let backgroundClass = "clear"; //기본값
+  // 날씨 데이터가 존재하지 않을 경우 빈 화면을 반환
+  if (!data) {
+    return null;
+  }
 
-    switch (Math.floor(data.id / 100)) {
-      case 2:
-        backgroundClass = "thunderstorm";
-        break;
-      case 3:
-        backgroundClass = "drizzle";
-        break;
-      case 5:
-        backgroundClass = "rain";
-        break;
-      case 6:
-        backgroundClass = "snow";
-        break;
-      case 7:
-        backgroundClass = "atmosphere";
-        break;
-      case 8:
-        if (data.weather[0].id === 800) {
-          backgroundClass = "clear";
-        } else {
-          backgroundClass = "clouds";
-        }
-        break;
-      default:
-        backgroundClass = "clear"; // 기본값
-    }
+  //날씨에 따라 백그라운드 이미지 변경
+  let backgroundClass = "clear"; //기본값
+  let newDessert = [];
 
-    return (
-      <div className={`weather_dessert_section ${backgroundClass}`}>
-        <div className="weather_dessert_wrapper">
+  switch (Math.floor(data.id / 100)) {
+    case 2:
+      backgroundClass = "thunderstorm";
+      newDessert = ["핫초코", "쿠키", "스콘", "뱅쇼"];
+      break;
+    case 3:
+      backgroundClass = "drizzle";
+      newDessert = ["라떼", "쿠키", "스콘", "브라우니"];
+      break;
+    case 5:
+      backgroundClass = "rain";
+      newDessert = ["라떼", "쿠키", "스콘", "브라우니"];
+      break;
+    case 6:
+      backgroundClass = "snow";
+      newDessert = ["핫초코", "쿠키", "스콘", "뱅쇼"];
+      break;
+    case 7:
+      backgroundClass = "atmosphere";
+      newDessert = ["타르트", "브라우니", "라떼", "주스"];
+      break;
+    case 8:
+      if (data.id === 800) {
+        backgroundClass = "clear";
+        newDessert = ["케이크", "아이스크림", "빙수", "마카롱"];
+      } else {
+        backgroundClass = "clouds";
+        newDessert = ["브라우니", "아이스크림", "빙수", "크로플"];
+      }
+      break;
+    default:
+      backgroundClass = "clear"; // 기본값
+  }
+
+  return (
+    <div className={`weather_dessert_section ${backgroundClass}`}>
+      <div className="weather_dessert_wrapper">
+        <div className="weather_dessert_title">
           <h2>지금 날씨엔 이 디저트 어떠세요? ({data.main})</h2>
-          <Slider responsive={responsive} />
+          <p>
+            {newDessert.map((dessert, index) => (
+              <span key={index}>#{dessert} </span>
+            ))}
+          </p>
         </div>
+
+        <Slider responsive={responsive} />
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default WeatherDessertSlide;

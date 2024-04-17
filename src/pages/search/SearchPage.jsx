1b217da7a1a593ae786d6b314bc3d07f3a7d8282
Jihee <sjh709@naver.com>
2024-04-17 16:11:41 +0900

    search : 지도 마커 표시

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./SearchPage.style.css";
 import { useSearchMapQuery } from "../../hooks/useSearchMap";
 import { useSelector } from "react-redux";
 
 const SearchPage = () => {
+  const { kakao } = window;
   const { latitude, longitude } = useSelector(
     (state) => state.search?.location
   );
   const { data, isLoading, isError, error } = useSearchMapQuery();
   console.log("data!!", data);
+
+  const displayMarker = (data) => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new kakao.maps.LatLng(latitude, longitude),
+      level: 1,
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    // 현재 위치 마커 표시
+    const markerPosition = new kakao.maps.LatLng(latitude, longitude);
+    const imageSrc = `https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png`;
+    const imageSize = new kakao.maps.Size(24, 35);
+    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+    const currentMarker = new kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImage,
+    });
+
+    // 지도 범위 재설정
+    const bounds = new kakao.maps.LatLngBounds();
+    bounds.extend(markerPosition);
+
+    // 여러개의 마커 표시
+    for (let i = 0; i < data.length; i++) {
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(data[i].y, data[i].x),
+        title: data[i].place_name,
+      });
+      bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+    }
+
+    map.setBounds(bounds);
+    currentMarker.setMap(map);
+  };
+
+  useEffect(() => {
+    if (data) {
+      kakao.maps.load(() => displayMarker(data));
+    }
+  }, [data]);
 
   if (isLoading) {
     return <div className="search_map_spinner"></div>;
@@ -38,7 +82,7 @@ const SearchPage = () => {
             <div>{/* <Card/> */}</div>
           </div>
         </section>
-        <section className="search_map">map</section>
+        <section id="map" className="search_map"></section>
       </div>
     </div>
   );

c39913aed44e319fe5286c2af532d01c49671f93
hayoung <ha02e@naver.com>
2024-04-17 16:24:03 +0900

    searchApi.js 생성

---
+++ src/utils/searchApi.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_SEARCH_API_KEY;
+
+const searchApi = axios.create({
+  baseURL: "https://dapi.kakao.com/v2/local/search",
+  headers: {
+    Accept: "application/json",
+    Authorization: `KakaoAK ${API_KEY}`,
+  },
+});
+
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 추가하기
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default searchApi;

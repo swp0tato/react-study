d326ee9233c6230adef8f68802fb8d17785c3b32
jay0425 <heedev94@gmail.com>
2024-04-19 11:34:25 +0900

    모바일 버전 css 수정, address 가져오기 구현

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -33,7 +33,6 @@ const Detail = () => {
       {/* <div className="detail-subject">영주시 디저트 카페</div> */}
       <h1 className="detail-title">{id}</h1>
       <p className="detail-address">{address ? address : '주소없음'}</p>
-      <p className="detail-address">{address}</p>
 
       <InfiniteScroll
         pageStart={0}

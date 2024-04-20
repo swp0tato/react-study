42a2a20337d0e10b2a3aa7b87dd853ea7fc97386
minsol <rlawlgus3954@naver.com>
2024-04-20 23:24:57 +0900

    로그인ui수정

--- src/components/Authenticate/SignInForm/SignInForm.jsx
+++ src/components/Authenticate/SignInForm/SignInForm.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./SignInForm.style.css";
-import AuthInput from "../../../common/Authenticate/AuthInput/AuthInput";
-import { useNavigate } from "react-router-dom";
-import AuthTextBtn from "../../../common/Authenticate/AuthTextBtn/AuthTextBtn";
+import React from 'react';
+import './SignInForm.style.css';
+import AuthInput from '../../../common/Authenticate/AuthInput/AuthInput';
+import { useNavigate } from 'react-router-dom';
+import AuthTextBtn from '../../../common/Authenticate/AuthTextBtn/AuthTextBtn';
 
 const SignInForm = ({ setEmail, email, setPassword, password }) => {
   const navigate = useNavigate();
@@ -15,15 +15,15 @@ const SignInForm = ({ setEmail, email, setPassword, password }) => {
   };
 
   const emailProps = {
-    type: "email",
-    placeholder: "이메일",
+    type: 'email',
+    placeholder: '이메일',
     handleSetValue: (e) => setEmail(e.target.value),
     isValue: email,
   };
 
   const passwordProps = {
-    type: "password",
-    placeholder: "비밀번호",
+    type: 'password',
+    placeholder: '비밀번호',
     handleSetValue: (e) => setPassword(e.target.value),
     isValue: password,
   };
@@ -34,11 +34,19 @@ const SignInForm = ({ setEmail, email, setPassword, password }) => {
       <AuthInput {...passwordProps} />
       <div
         className="sign_up_wrapper"
-        style={{ display: "flex", justifyContent: "center" }}
+        style={{ display: 'flex', justifyContent: 'center' }}
       >
-        <AuthTextBtn onClick={naviateToSignUP}>회원가입</AuthTextBtn>
-        <div>|</div>
-        <AuthTextBtn onClick={navigateToPassword}>비밀번호 재발급</AuthTextBtn>
+        <div className="join-text-wrap">
+          <div>
+            <AuthTextBtn onClick={naviateToSignUP}>회원가입</AuthTextBtn>
+          </div>
+          <div className="bar-wrap">|</div>
+          <div>
+            <AuthTextBtn onClick={navigateToPassword}>
+              비밀번호 재발급
+            </AuthTextBtn>
+          </div>
+        </div>
       </div>
     </div>
   );

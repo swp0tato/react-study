3bd08d1743fcabae2c477bd8af550155ac8912bf
yeonseo <yeonseo328@gmail.com>
2024-04-20 20:11:23 +0900

    해시태그 작업

--- src/pages/board/component/Reply.jsx
+++ src/pages/board/component/Reply.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   addDoc,
   collection,
@@ -8,22 +8,22 @@ import {
   getDocs,
   deleteDoc,
   doc,
-} from 'firebase/firestore';
-import { db } from '../../../firebase';
-import './Reply.style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+} from "firebase/firestore";
+import { db } from "../../../firebase";
+import "./Reply.style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Reply = ({ boardId }) => {
-  const [commentText, setCommentText] = useState('');
+  const [commentText, setCommentText] = useState("");
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
     const fetchComments = async () => {
       try {
         const q = query(
-          collection(db, 'reply'),
-          where('boardId', '==', boardId),
+          collection(db, "reply"),
+          where("boardId", "==", boardId)
         );
         const querySnapshot = await getDocs(q);
         const commentsData = querySnapshot.docs.map((doc) => ({
@@ -32,7 +32,7 @@ const Reply = ({ boardId }) => {
         }));
         setComments(commentsData);
       } catch (error) {
-        console.error('댓글을 불러오는 중 오류가 발생했습니다:', error);
+        console.error("댓글을 불러오는 중 오류가 발생했습니다:", error);
       }
     };
 
@@ -42,7 +42,7 @@ const Reply = ({ boardId }) => {
   const handleSubmitComment = async () => {
     try {
       if (!commentText) {
-        alert('댓글을 입력해주세요.');
+        alert("댓글을 입력해주세요.");
         return;
       }
 
@@ -53,33 +53,33 @@ const Reply = ({ boardId }) => {
         createdAt: serverTimestamp(),
       };
 
-      const docRef = await addDoc(collection(db, 'reply'), commentData);
+      const docRef = await addDoc(collection(db, "reply"), commentData);
       const newComment = { id: docRef.id, ...commentData };
 
       // 상태 업데이트: 최신순이 위로 올라오게 설정
       setComments((prevComments) => [newComment, ...prevComments]);
 
-      alert('댓글이 등록되었습니다!');
+      alert("댓글이 등록되었습니다!");
 
-      setCommentText('');
+      setCommentText("");
     } catch (error) {
-      console.error('댓글을 추가하는 도중 오류가 발생했습니다:', error);
+      console.error("댓글을 추가하는 도중 오류가 발생했습니다:", error);
     }
   };
 
   const handleDeleteComment = async (commentId) => {
-    console.log('Deleting comment with ID:', commentId);
+    console.log("Deleting comment with ID:", commentId);
     try {
-      await deleteDoc(doc(db, 'reply', commentId));
+      await deleteDoc(doc(db, "reply", commentId));
 
       // 댓글 삭제 후 상태 업데이트
       setComments((prevComments) =>
-        prevComments.filter((comment) => comment.id !== commentId),
+        prevComments.filter((comment) => comment.id !== commentId)
       );
 
-      alert('댓글이 삭제되었습니다!');
+      alert("댓글이 삭제되었습니다!");
     } catch (error) {
-      console.error('댓글을 삭제하는 도중 오류가 발생했습니다:', error);
+      console.error("댓글을 삭제하는 도중 오류가 발생했습니다:", error);
     }
   };
 
@@ -91,24 +91,28 @@ const Reply = ({ boardId }) => {
         onChange={(e) => setCommentText(e.target.value)}
         placeholder="댓글을 작성해보아요 : )"
       />
-      <button className="reply-btn" onClick={handleSubmitComment}>
-        등록
-      </button>
+      <div className="reply-btn-wrap">
+        <button className="reply-btn" onClick={handleSubmitComment}>
+          등록
+        </button>
+      </div>
 
       <div className="reply-list">
         <div className="comment-count">댓글 ({comments.length})</div>
         {comments.map((comment) => (
           <div key={comment.id} className="reply-item">
-            <p>
-              <FontAwesomeIcon icon={faUser} className="reply-user-icon" />{' '}
-              {comment.text}
+            <div className="reply-wrap">
+              <div>
+                <FontAwesomeIcon icon={faUser} className="reply-user-icon" />{" "}
+                {comment.text}
+              </div>
               <button
                 className="reply-delete-btn"
                 onClick={() => handleDeleteComment(comment.id)}
               >
                 삭제
               </button>
-            </p>
+            </div>
           </div>
         ))}
       </div>

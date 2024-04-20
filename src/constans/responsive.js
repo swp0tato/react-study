4586f323f6750c6f0b2ec1808885e81a5e4b2c03
hayoung <ha02e@naver.com>
2024-04-20 17:48:30 +0900

    responsive 수정

--- src/constans/responsive.js
+++ src/constans/responsive.js
@@ -2,12 +2,12 @@ export const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 2000 },
     items: 5,
-    slidesToSlide: 1,
+    slidesToSlide: 5,
   },
   tablet: {
     breakpoint: { max: 2000, min: 464 },
     items: 3,
-    slidesToSlide: 1,
+    slidesToSlide: 3,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
